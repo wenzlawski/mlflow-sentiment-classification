4720e2dab10accc3f35026b722c7b638f0ced227
Marc Wenzlawski <marc.wenzlawski@outlook.de>
2023-03-16 14:14:45 +0000

    update train job

--- train.sh
+++ train.sh
@@ -5,12 +5,15 @@
 
 #$ -pe smp.pe 8
 
-module load apps/binapps/anaconda3/2022.10
+#module load apps/binapps/anaconda3/2022.10
+module load apps/binapps/anaconda3/2020.07
 module load apps/binapps/pytorch/1.11.0-39-gpu-cu113
-module load tools/env/proxy
+module load tools/env/proxy2
 
 export OMP_NUM_THREADS=$NSLOTS
 
-conda install mlflow
+conda create -y -n mlflow python=3.8 -c conda-forge mlflow
+
+source activate mlflow
 
 mlflow run . -P accelerator=gpu
